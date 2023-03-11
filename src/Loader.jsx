5043a21097d81c3ecc2ab11fbdbeee7b1a144ca8
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2023-03-11 22:03:35 +0100

    model catalog created

--- src/Loader.jsx
+++ src/Loader.jsx
@@ -1,14 +1,17 @@
 import { useLoader, useFrame } from '@react-three/fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import React, {useRef} from 'react';
+import React, {useRef,useState} from 'react';
 
 export const Shaft=()=>{
-    const gltf = useLoader(GLTFLoader, '/sprzeglo_klowe_for_app.glb')
+    const gltf = useLoader(GLTFLoader, '/models/sprzeglo_klowe_for_app.glb')
     const mesh = useRef()
-    //useFrame((state, delta) => (mesh.current.rotation.x -= delta))
+    const [motion,setMotion]= useState(true)
+    
+    const ifMotion = motion ? 1 : 0
+    useFrame((state, delta) => (mesh.current.rotation.x -=ifMotion*delta))
     return(
         <>
-        <primitive object={gltf.scene} dispose={null} ref={mesh}/>
+        <primitive object={gltf.scene} dispose={null} ref={mesh} onPointerDown={(e) =>{ setMotion(!motion); console.log('clicked')}}/>
         </>
     )
 }
